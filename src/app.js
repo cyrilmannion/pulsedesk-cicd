fbb1f7e572f072cef8364a50ed9e55ed3135fb77
cyrilmannion <cyrilmannion@gmail.com>
2026-05-22 19:54:03 +0100

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,9 @@
+// src/app.js
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("CI/CD Pipeline Running Successfully!");
+});
+
+module.exports = app;
