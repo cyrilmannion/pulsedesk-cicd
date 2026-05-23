a3fd2625756e484c9d14c71ffe7599e805ffa474
cyrilmannion <cyrilmannion@gmail.com>
2026-05-23 14:36:17 +0100

    Break Test

--- tests/app.test.js
+++ tests/app.test.js
@@ -4,5 +4,5 @@ const app = require("../src/app");
 
 test("GET / should return 200", async () => {
     const res = await request(app).get("/");
-    expect(res.statusCode).toBe(200);
+    expect(res.statusCode).toBe(500);// wrong on purpose to show how to fix it
 });
