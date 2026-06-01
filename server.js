9c608619b99ed708cf6cd768cee02788ec19c483
cyrilmannion <cyrilmannion@gmail.com>
2026-06-01 12:31:17 +0100

    Testing rolling deployment on two instances

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = 3000;
 
 app.get('/', (req, res) => {
-  res.send('PulseDesk app running ✅');
+  res.send('PulseDesk app running now, well done');
 });
 
 app.get('/health', (req, res) => {
